d939e334a3c1dc078ff82c68ff4bea29b3f74503
leoCardosoDev <leocardosodev@gmail.com>
2023-10-06 09:13:14 -0300

    feat: desafio 008

---
+++ exercicios/008_desafio.py
@@ -0,0 +1,6 @@
+mt = int(input('Escreve um número que represento o metro: '))
+cm = mt / 100
+mi = mt / 1000
+
+print('O valor de {}mt são {}cm!'.format(mt, cm))
+print('O valor de {}mt são {}mi'.format(mt, mi))
