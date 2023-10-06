6b47333bfcaf6bdadb86992eb76b0ea601b962d3
leoCardosoDev <leocardosodev@gmail.com>
2023-10-06 15:08:40 -0300

    feat: desafios 014 com mais uma formula

--- exercicios/014_desafio.py
+++ exercicios/014_desafio.py
@@ -1,3 +1,5 @@
 c = float(input('Informe a temperatura em °C: '))
 f = c * 1.8 + 32
+f2 = 9 * c / 5 + 32
 print('A temperatura de {}°C corresponte a {}°F!'.format(c, f))
+print('A temperatura de {}°C corresponte a {}°F!'.format(c, f2))
