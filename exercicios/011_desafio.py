3d80f5d162ad4ba4025bf05ead752b71374284b4
leoCardosoDev <leocardosodev@gmail.com>
2023-10-06 09:50:35 -0300

    feat: desafio 011 - b

--- exercicios/011_desafio.py
+++ exercicios/011_desafio.py
@@ -2,4 +2,4 @@ alt = float(input('Digite a altura: '))
 lag = float(input('Digite a largura: '))
 
 lt = (alt * lag) / (2**2)
-print('Você precisará de {}lt de tintas para pintar a área'.format(lt))
+print('Você precisará de {:.2f}lt de tintas para pintar a área'.format(lt))
