fcce535c4eeb8d860765a8bcae2bc1d3b5e948cd
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 14:25:09 -0300

    feat: desafios 026 corrigido

--- exercicios/026_desafio.py
+++ exercicios/026_desafio.py
@@ -1,4 +1,4 @@
-frase = str(input('Digite uma frase completa: '))
-print('A letra "A" aparece {} vezes'.format(frase.upper().strip().count('A')))
+frase = str(input('Digite uma frase completa: ')).upper().strip()
+print('A letra "A" aparece {} vezes'.format(frase.count('A')))
 print(frase.find('a'))
 print(frase.rfind('a'))
