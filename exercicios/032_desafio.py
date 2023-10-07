b6bdff6ca80ea496f992657a568d5685f1dc9111
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 17:22:45 -0300

    feat: desafios 032 corrigido

--- exercicios/032_desafio.py
+++ exercicios/032_desafio.py
@@ -1,5 +1,8 @@
+from datetime import date
 ano = int(input('Digite um ano: '))
-if ano % 4 == 0:
-    print("{} foi um ano BISSEXTO".format(ano))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print("O ano de {} foi um ano BISSEXTO".format(ano))
 else:
-    print("{} NÃO foi um ano BISSEXTO".format(ano))
+    print("O ano de {} NÃO foi um ano BISSEXTO".format(ano))
