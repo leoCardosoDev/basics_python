83b77177cf8f4db6230f5ac613369f413c72a9d7
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 17:40:33 -0300

    feat: desafios 033 corrigido

--- exercicios/033_desafio.py
+++ exercicios/033_desafio.py
@@ -2,20 +2,16 @@ n1 = int(input('DIgite um número: '))
 n2 = int(input('DIgite outro número: '))
 n3 = int(input('DIgite outro número: '))
 
-if n1 > n2 and n1 > n3:
-    print('{} é maior digitado'.format(n1))
+maior = n1
+menor = n1
 
 if n2 > n1 and n2 > n3:
-    print('{} é maior digitado'.format(n2))
-
+    maior = n2
 if n3 > n1 and n3 > n2:
-    print('{} é maior digitado'.format(n3))
-
-if n1 < n2 and n1 < n3:
-    print('{} é menor digitado'.format(n1))
-
+    maior = n3
 if n2 < n1 and n2 < n3:
-    print('{} é menor digitado'.format(n2))
-
+    menor = n2
 if n3 < n1 and n3 < n2:
-    print('{} é menor digitado'.format(n3))
+    menor = n3
+print('O menor valor digitado foi: {} é menor digitado'.format(menor))
+print('O maior valor digitado foi: {} é menor digitado'.format(maior))
