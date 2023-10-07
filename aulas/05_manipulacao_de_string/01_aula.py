a6e36a330eb0c658b45a74a4217e217131a85161
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 06:27:58 -0300

    feat: aulas manipulação de string

---
+++ aulas/05_manipulacao_de_string/01_aula.py
@@ -0,0 +1,14 @@
+frase = 'Curso em Video Python'
+print(frase)
+print(frase[3])
+print(frase[3:13])
+print(frase[:13])
+print(frase[13:])
+print(frase[1:15])
+print(frase[1:15:2])
+print(frase[1::2])
+print(frase[::2])
+
+print("""Do exercitation anim deserunt sint. Culpa aliquip officia ipsum eiusmod amet dolore commodo est qui ut fugiat. 
+Id amet aute mollit sit pariatur fugiat nostrud fugiat laborum mollit quis. Duis elit cillum adipisicing ea incididunt
+non labore dolor nostrud duis deserunt pariatur.""")
