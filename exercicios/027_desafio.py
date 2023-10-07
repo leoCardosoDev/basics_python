8927feb5cb9f022f0358bdca58c9feb472f80272
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 09:23:17 -0300

    feat: desafios 027

---
+++ exercicios/027_desafio.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite o seu nome completo: '))
+nomes = nome.split()
+lastname = nome.split()
+inverter = nomes.reverse()
+
+# print(nomes[0])
+print(nomes[0])
+print(lastname[0])
