503f3d08efdaf7a7357c7f388fbbd7af426b062d
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 06:46:26 -0300

    feat: aulas manipulação de string 2

---
+++ aulas/05_manipulacao_de_string/02_aula.py
@@ -0,0 +1,15 @@
+frase = 'Curso em Video Python'
+frase2 = '   Curso em Video Python    '
+print(frase.count('o'))
+print(frase.count('O'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(frase2)
+print(frase2.strip())
+
+print(frase.replace('Python', 'Android'))
+print('Curso' in frase)
+print('Android' in frase)
+print(frase.find('Curso'))
+print(frase.find('Android'))
+print(frase2.upper().strip().split())
