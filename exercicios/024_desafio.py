fdae0f7b7bdae2fd096cb4b99fc42dea78922d4d
leoCardosoDev <leocardosodev@gmail.com>
2023-10-07 14:15:53 -0300

    feat: desafios 024 corrigido

--- exercicios/024_desafio.py
+++ exercicios/024_desafio.py
@@ -1,3 +1,3 @@
-cidade = str(input('Digite o nome da sua cidade ou Estados: '))
+cidade = str(input('Digite o nome da sua cidade ou Estados: ')).strip().lower()
 santo = cidade.split()
-print('Santo' in santo[0])
+print('Santo' in santo[0].title())
