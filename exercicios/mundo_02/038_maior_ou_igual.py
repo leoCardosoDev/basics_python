c4bf64bd8a68ceddd370e89b3ad5e2146d0f8904
leoCardosoDev <leocardosodev@gmail.com>
2023-10-08 06:45:30 -0300

    feat: desafios 038

---
+++ exercicios/mundo_02/038_maior_ou_igual.py
@@ -0,0 +1,9 @@
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+if valor1 > valor2:
+  print('O primeiro valor é maior!')
+elif valor2 > valor1:
+  print('O segundo valor é maior!')
+else:
+  print('Não existe valor maior! Os dois são iguais!')
