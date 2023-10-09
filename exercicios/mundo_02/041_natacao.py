116f57d153c273b59fb045faa8b84cdd705b1a9c
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 09:42:48 -0300

    feat: desafios 041 arrumando a lógica

--- exercicios/mundo_02/041_natacao.py
+++ exercicios/mundo_02/041_natacao.py
@@ -4,11 +4,11 @@ idade = date.today().year - ano
 
 if idade <= 9:
     print('Você tem {} anos, então você faz parte da categoria MIRIM'.format(idade))
-elif idade > 9 and idade <= 14:
+elif idade <= 14:
     print('Você tem {} anos, então você faz parte da categoria INFANTIL'.format(idade))
-elif idade > 14 and idade <= 19:
+elif idade <= 19:
     print('Você tem {} anos, então você faz parte da categoria JUNIOR'.format(idade))
-elif idade > 19 and idade <= 20:
+elif idade <= 25:
     print('Você tem {} anos, então você faz parte da categoria SÊNIOR'.format(idade))
 else:
     print('Você tem {} anos, então você faz parte da categoria MASTER'.format(idade))
