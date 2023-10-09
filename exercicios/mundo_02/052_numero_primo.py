9280c51dd633b5112ed70d2377087580181256b9
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 18:20:19 -0300

    feat: desafios 052 - rsultado errado

---
+++ exercicios/mundo_02/052_numero_primo.py
@@ -0,0 +1,12 @@
+from math import sqrt
+n = int(input('Digite um número: '))
+if n == 2:
+    print(f'{n} é um número primo')
+if n > 1:
+    for p in range(3, int(sqrt(n)) + 1, 2):
+        if n % p == 1:
+            print(f'{n} é um número primo')
+        else:
+            print(f'{n} não é um número primo')
+else:
+    print(f'{n} não é um número primo')
