f69dc5fbdb1ae431fe1e601fc154d19deb0c40d6
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 11:23:50 -0300

    feat: aula - lacos parte 1

---
+++ aulas/mundo_02/02_estrutura-de_repeticao_for/01_aula.py
@@ -0,0 +1,21 @@
+for c in range(0, 6):
+    print(f'{c} - oi')
+print('FIM')
+
+for c in range(6, 0, -1):
+    print(f'{c} - oi')
+print('FIM')
+
+for c in range(0, 7, 2):
+    print(f'{c} - oi')
+print('FIM')
+
+n = int(input('Digite um número: '))
+for c in range(0, n):
+    print(c)
+print('Fim')
+
+n2 = int(input('Digite outro número: '))
+for c in range(0, n2+1):
+    print(c)
+print('Fim')
