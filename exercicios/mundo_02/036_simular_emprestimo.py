f26a3c0a556cd4f154a99bb21155a720d5d23d31
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 08:11:06 -0300

    feat: desafios 036 com outra lógica

--- exercicios/mundo_02/036_simular_emprestimo.py
+++ exercicios/mundo_02/036_simular_emprestimo.py
@@ -3,6 +3,7 @@ salarioComprador = float(input('Qual é o salário do comprador? R$: '))
 qtdAnos = int(input('Em quantos anos pretende pagar? '))
 parcelas = valorDaCasa / (qtdAnos * 12)
 trintaPorCento = salarioComprador - (salarioComprador * 30 / 100)
+# minimo = salarioComprador * 30 / 100
 
 if parcelas > trintaPorCento - parcelas:
  print('Para pagar uma casa de R$ {:.2f} em {} anos, a prestação será de R$ {:.2f}\n EMPRESTIMO NEGADO! '.format(valorDaCasa, qtdAnos, parcelas))
