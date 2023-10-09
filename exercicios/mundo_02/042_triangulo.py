4f5886a4bcabc7281e462eb0aea9d775e0823d3b
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 09:58:24 -0300

    feat: desafios 042 corrigindo um bug de lógica e ajustando nova lógica do python

--- exercicios/mundo_02/042_triangulo.py
+++ exercicios/mundo_02/042_triangulo.py
@@ -3,9 +3,11 @@ reta2 = int(input('Digite o cumprimento de um outro lado do triângulo: '))
 reta3 = int(input('Digite o cumprimento de um outro lado do triângulo: '))
 
 ehUmTriangulo = (reta1 + reta2) > reta3 and (reta1 + reta3) > reta2 and (reta3 + reta2) > reta1
+# ehUmTriangulo = reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1  + reta2
 equilatero = reta1 == reta2 and reta2 == reta3 and reta3 == reta1
+# equilatero = reta1 == reta2 == reta3
 isosceles = (reta1 == reta2 and reta1 != reta3) or (reta2 == reta3 and reta2 != reta1) or (reta3 == reta1 and reta3 != reta2)
-
+#escaleno = reta1 != reta2 != reta3 != reta1
 if ehUmTriangulo:
     if equilatero:
         print('É um triângulo Equilatero')
