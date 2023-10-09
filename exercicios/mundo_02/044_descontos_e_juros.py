a361cf3ae49765402d46ab7f888d5b5ed0803ea0
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 10:29:03 -0300

    feat: desafios 044 ajustando para parcelas

--- exercicios/mundo_02/044_descontos_e_juros.py
+++ exercicios/mundo_02/044_descontos_e_juros.py
@@ -1,14 +1,23 @@
 preco = float(input('Qual o valor do produto? R$ '))
-pagamento = str(input('Qual vai ser a forma de pagamento? '))
+pagamento = int(input('''Qual vai ser a forma de pagamento?:
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão
+'''))
+if pagamento == 4: 
+  parcelas = int(input('Quantas parcelas? '))
 
-if pagamento == 'dinheiro':
+if pagamento == 1:
   precoFinal = preco - preco * 10 / 100
   print('Você recebeu 10% de desconto pelo pagamento em {}! Então o seu produto de R$ {:.2f} ficará por R${:.2f}'.format(pagamento, preco, precoFinal))
-elif pagamento == 'cartão':
+elif pagamento == 2:
   precoFinal = preco - preco * 5 / 100
   print('Você recebeu 5% de desconto pelo pagamento no {}! Então o seu produto de R$ {:.2f} ficará por R$ {:.2f}'.format(pagamento, preco, precoFinal))
-elif pagamento == '2x':
+elif pagamento == 3:
   print('Você não recebeu desconto pelo parcelamento {} no cartão! Então o seu produto de R$ {:.2f} ficará por R$ {:.2f}'.format(pagamento, preco, preco))
 else:
   precoFinal = preco + preco * 20 / 100
-  print('Você vai pagar 20% de juros pelo parcelamento {}! Então o seu produto de R$ {:.2f} ficará por R$ {:.2f}'.format(pagamento, preco, precoFinal))
+  parcela = precoFinal / parcelas
+  print(f'Sua compra será parcelada em {parcelas}x de R$ {parcela:.2f} COM JUROS')
+  print('Sua compra de R$ {:.2f} vai custar R$ {:.2f}'.format(preco, precoFinal))
