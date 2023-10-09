170c1434de3db517c04348817be715812ba85e01
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 05:50:01 -0300

    feat: desafios 043

---
+++ exercicios/mundo_02/043_imc.py
@@ -0,0 +1,18 @@
+peso = float(input('Qual o seu peso? Kg: '))
+altura = float(input('Qual a sua altura? '))
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print('Abaixo do peso com {:.2f} de Indice de massa corporal'.format(imc))
+elif imc > 18.5 and imc < 25:
+    print(
+        'Você está no peso ideal com {:.2f} de Indice de massa corporal'.format(imc))
+elif imc > 25 and imc < 30:
+    print(
+        'Você está com sobrepeso com {:.2f} de Indice de massa corporal'.format(imc))
+elif imc > 30 and imc < 40:
+    print(
+        'Você está com obesidade grau II com {:.2f} de Indice de massa corporal'.format(imc))
+else:
+    print(
+        'Você está com obesidade grau III com {:.2f} de Indice de massa corporal'.format(imc))
