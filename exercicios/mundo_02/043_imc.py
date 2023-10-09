06b6ba0d1dadad02f9cc8dfd68ac098471322916
leoCardosoDev <leocardosodev@gmail.com>
2023-10-09 10:10:35 -0300

    feat: desafios 043 corrigindo a pontencialização

--- exercicios/mundo_02/043_imc.py
+++ exercicios/mundo_02/043_imc.py
@@ -1,18 +1,14 @@
 peso = float(input('Qual o seu peso? Kg: '))
 altura = float(input('Qual a sua altura? '))
-imc = peso / (altura * altura)
+imc = peso / (altura ** 2)
 
 if imc < 18.5:
     print('Abaixo do peso com {:.2f} de Indice de massa corporal'.format(imc))
-elif imc < 25:
-    print(
-        'Você está no peso ideal com {:.2f} de Indice de massa corporal'.format(imc))
-elif imc < 30:
-    print(
-        'Você está com sobrepeso com {:.2f} de Indice de massa corporal'.format(imc))
-elif imc < 40:
-    print(
-        'Você está com obesidade grau II com {:.2f} de Indice de massa corporal'.format(imc))
+elif 18.5 <= imc < 25:
+    print('Você está no peso ideal com {:.2f} de Indice de massa corporal'.format(imc))
+elif 25 <= imc < 30:
+    print('Você está com sobrepeso com {:.2f} de Indice de massa corporal'.format(imc))
+elif 30 <= imc < 40:
+    print('Você está com obesidade grau II com {:.2f} de Indice de massa corporal'.format(imc))
 else:
-    print(
-        'Você está com obesidade grau III com {:.2f} de Indice de massa corporal'.format(imc))
+    print('Você está com obesidade grau III com {:.2f} de Indice de massa corporal'.format(imc))
