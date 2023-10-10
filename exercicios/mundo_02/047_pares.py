1fe98a3c0bbb57a84aef6d2e34b52eff18951f51
leoCardosoDev <leocardosodev@gmail.com>
2023-10-10 07:38:49 -0300

    fix: desafios 047 ajuste

--- exercicios/mundo_02/047_pares.py
+++ exercicios/mundo_02/047_pares.py
@@ -1,4 +1,9 @@
 for c in range(1, 51):
     if c % 2 == 0:
-        print(f'{c}')
+        print(f'{c}', end=', ')
+print('FIM')
+
+# nova lógica
+for c in range(2, 51, 2):
+    print(f'{c}', end=', ')
 print('FIM')
