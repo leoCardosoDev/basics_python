cdf2a982deee4d3189f8ac7419586e1b11601694
leoCardosoDev <leocardosodev@gmail.com>
2023-10-10 07:58:27 -0300

    fix: desafios 050 totalmente corrigido

--- exercicios/mundo_02/050_somar_pares.py
+++ exercicios/mundo_02/050_somar_pares.py
@@ -1,6 +1,15 @@
-for c in range(0, 6):
-    n = int(input('Digite um numero: '))
-    if n % 2 == 0:
-        par = n
-        print(f'{n} + {par} = {par + n }')
-print('FIM')
+# for c in range(0, 6):
+#     n = int(input('Digite um numero: '))
+#     if n % 2 == 0:
+#         par = n
+#         print(f'{n} + {par} = {par + n }')
+# print('FIM')
+
+soma = 0
+cont = 0
+for c in range(0,6):
+  n = int(input('Digite um número: '))
+  if n % 2 == 0:
+    soma += n
+    cont+= 1
+print(f'Você informou {cont} números pares e a soma foi {soma}')
