28900174467b0eb3139b3ed15024efaa42757a52
leoCardosoDev <leocardosodev@gmail.com>
2023-10-10 07:45:14 -0300

    fix: desafios 048 ajuste

--- exercicios/mundo_02/048_multiplo_de_tres.py
+++ exercicios/mundo_02/048_multiplo_de_tres.py
@@ -1,5 +1,7 @@
 m3 = 0
-for c in range(1, 500):
+cont = 0
+for c in range(1, 501, 2):
     if c % 3 == 0:
+        cont+=1
         m3 += c
-print(f'A soma de todos os números multiplos de 3 de 1 a 500 é: {m3}')
+print(f'A soma de todos os {cont} multiplos de 3 é: {m3}')
