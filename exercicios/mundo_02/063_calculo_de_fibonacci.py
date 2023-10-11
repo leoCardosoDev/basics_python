af442fefedf023b51befdf1c21c521dc79b58a3f
leoCardosoDev <leocardosodev@gmail.com>
2023-10-11 10:27:58 -0300

    feat: desafio 063 certo mas outra proposta

--- exercicios/mundo_02/063_calculo_de_fibonacci.py
+++ exercicios/mundo_02/063_calculo_de_fibonacci.py
@@ -7,3 +7,17 @@ while n != 0:
     a = b
     b = c
     n -= 1
+
+# Proposta Guanabara
+num = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print(f'{t1} -> {t2}', end='')
+cont = 3
+while cont <= num:
+    t3 = t1 + t2
+    print(f'-> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print('FIM')
