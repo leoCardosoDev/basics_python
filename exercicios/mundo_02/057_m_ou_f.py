490475f8276a97a45cd7ea41c9d200065d2e9c8b
leoCardosoDev <leocardosodev@gmail.com>
2023-10-11 08:53:22 -0300

    feat: desafio 057 com a logica de python

--- exercicios/mundo_02/057_m_ou_f.py
+++ exercicios/mundo_02/057_m_ou_f.py
@@ -1,4 +1,6 @@
-g = str(input('Qual o seu Sexo? [M/F]: ')).strip().upper()
+g = str(input('Qual o seu Sexo? [M/F]: ')).strip().upper()[0]
 while g != 'M' and g != 'F':
     g = str(input('Você digitou errado! Tenta os valores M ou F: ')).strip().upper()
 print('FIM')
+
+# while g not in 'MmFf': // solução com python
