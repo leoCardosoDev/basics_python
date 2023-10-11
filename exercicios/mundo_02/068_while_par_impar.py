6b10f53ae083124c8023091129d737525c531efd
leoCardosoDev <leocardosodev@gmail.com>
2023-10-11 16:06:36 -0300

    feat: desafio 068

---
+++ exercicios/mundo_02/068_while_par_impar.py
@@ -0,0 +1,21 @@
+from random import randint
+
+ganhou = True
+cont = 0
+
+while True:
+  if ganhou != True:
+    break
+
+  computador = randint(1, 20)
+  jogador = int(input('Diga um valor: '))
+  soma = jogador + computador
+  parOuImpar = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+  ganhou = parOuImpar == 'P' and soma % 2 == 0 or parOuImpar == 'I' and soma % 2 == 1
+  
+  if ganhou:
+    print('Você GANHOU!')
+    cont += 1
+
+print('Você PERDEU!')
+print(f'GAME OVER! Você venceu {cont}')
