6376d11ca6940f3b4048fb38e103d29a78c936b7
leoCardosoDev <leocardosodev@gmail.com>
2023-10-11 15:22:56 -0300

    feat: desafio 066

---
+++ exercicios/mundo_02/067_tabuada_while_and_break.py
@@ -0,0 +1,9 @@
+multiplicador = 0
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
+        break
+    while multiplicador <= 10:
+        print(f'{n:>2} x {multiplicador:>2} = {n * multiplicador}')
+        multiplicador += 1
+    multiplicador = 0
