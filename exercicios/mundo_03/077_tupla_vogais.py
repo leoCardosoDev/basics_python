a5b807ef3ec98873fe0f71fc59f6478fcbb71789
leoCardosoDev <leocardosodev@gmail.com>
2023-10-12 20:10:40 -0300

    feat: desafio 077

---
+++ exercicios/mundo_03/077_tupla_vogais.py
@@ -0,0 +1,28 @@
+palavras = ('aprender', 'programar', 'linguagem')
+
+for palavra in palavras:
+    a = palavra.lower().find('a')
+    e = palavra.lower().find('e')
+    i = palavra.lower().find('i')
+    o = palavra.lower().find('o')
+    u = palavra.lower().find('u')
+
+    print(f'Na palavra {palavra.upper()} temos ', end='')
+
+    # Se a vogal estiver presente, imprima a vogal
+    if a != -1:
+        print('a ', end='')
+    if e != -1:
+        print('e ', end='')
+    if i != -1:
+        print('i ', end='')
+    if o != -1:
+        print('o ', end='')
+    if u != -1:
+        print('u ', end='')
+
+    # Se nenhuma vogal estiver presente, imprima 'nenhuma'
+    if a == e == i == o == u == -1:
+        print('nenhuma')
+    else:
+        print()
