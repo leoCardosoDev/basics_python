96fa50c69605e92acbc0af9883d7fa2765f571df
leoCardosoDev <leocardosodev@gmail.com>
2023-10-12 09:06:28 -0300

    feat: desafio ajuste

--- exercicios/mundo_02/069_pessoas.py
+++ exercicios/mundo_02/069_pessoas.py
@@ -7,18 +7,22 @@ while True:
     if continua not in 'Ss':
         break
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
-    continua = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    sexo = ' '
+    while sexo not in 'MF':
+      sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
+    continua = ' '
+    while continua not in 'SN':
+        continua = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
 
     if idade >= 18:
         somaIdadeMais18 += 1
 
     if sexo == 'M':
         totalHomens += 1
-    
+
     if sexo == 'F' and idade <= 19:
         mulheresMenor20 += 1
-        
+
 print(f'Total de pessoas com mais de 18 anos: {somaIdadeMais18}')
 print(f'Ao todo temos {totalHomens} homens cadastrados')
 print(f'E temos {mulheresMenor20} mulheres com menos de 20 anos')
