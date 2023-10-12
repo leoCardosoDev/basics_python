01e81bcfa6112cf2737f1808f546a631af34cc4d
leoCardosoDev <leocardosodev@gmail.com>
2023-10-12 17:33:27 -0300

    feat: desafio 073

---
+++ exercicios/mundo_03/073_tupla_brasileiro.py
@@ -0,0 +1,19 @@
+times = 'Botafogo', 'Bragantino', 'Grêmio', 'Palmeiras', 'Flamengo', 'Fortaleza', 'Fluminense', 'Athletico-PR', 'Atlético-MG', 'São Paulo', 'Cuiabá', 'Internacional', 'Corinthians', 'Santos', 'Cruzeiro', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'América-MG'
+print('-=' * 40)
+print('Lista dos times do Brasileirão: ', end='')
+print(times)
+
+print('-=' * 40)
+print('Os 5 primeiros: ', end='')
+print(times[:5])
+
+print('-=' * 40)
+print('Os quatros últimos: ', end='')
+print(sorted(times[-4:]))
+
+print('-=' * 40)
+print('Times em ordem alfabetica: ', end='')
+print(sorted(times))
+
+print('-=' * 40)
+print(f'O time {times[8]} está na 8º posição: ')
