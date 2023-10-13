e5ddd4697e1c7fddbd6c43471bf0e6a0c099d0cb
leoCardosoDev <leocardosodev@gmail.com>
2023-10-13 10:30:27 -0300

    feat: desfio 080

---
+++ exercicios/mundo_03/080_valores_insert.py
@@ -0,0 +1,23 @@
+valores = []
+for i in range(0, 5):
+    num = int(input('Digite um valor: '))
+    if i == 0:
+        valores.append(num)
+        print('Adicionado ao final da lista')
+    elif num > max(valores):
+        valores.append(num)
+        print('Adicionado ao final da lista')
+    elif num < min(valores):
+        valores.insert(0, num)
+        print(f'Valor adicionado na posição 0 da lista')
+    elif num > valores[0]:
+        valores.insert(1, num)
+        print(f'Valor adicionado na posição 1 da lista')
+    elif num > valores[1]:
+        valores.insert(2, num)
+        print(f'Valor adicionado na posição 3 da lista')
+    elif num > valores[2]:
+        valores.insert(3, num)
+        print(f'Valor adicionado na posição 2 da lista')
+
+print(f'Os valores digitados em ordem foram: {valores}')
