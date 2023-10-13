c04d474e17a061d2162a05b3f1a7fff26b6eaf29
leoCardosoDev <leocardosodev@gmail.com>
2023-10-13 05:08:26 -0300

    fix: desafio 072

--- exercicios/mundo_03/072_tupla_numero_extenso.py
+++ exercicios/mundo_03/072_tupla_numero_extenso.py
@@ -1,9 +1,9 @@
 extenso = 'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
-num = int(input('Digite um número de 0 a 20: '))
 
-if num > 20 or num < 0:
-    while True:
-        num = int(input('Tente novamente! Digite um número de 0 a 20: '))
-        if num <= 20 or num >= 0:
-            break
+while True:
+    num = int(input('Digite um número de 0 a 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente! ', end='')
+
 print(f'Você digitou {extenso[num]}')
