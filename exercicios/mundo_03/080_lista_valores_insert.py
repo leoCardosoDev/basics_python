3d250ab01b4e23ced32e437e8c6c78339297a91c
leoCardosoDev <leocardosodev@gmail.com>
2023-10-13 16:20:26 -0300

    fix: desfio 080 - corrigindo bug

--- exercicios/mundo_03/080_lista_valores_insert.py
+++ exercicios/mundo_03/080_lista_valores_insert.py
@@ -4,19 +4,19 @@ for i in range(0, 5):
     if i == 0:
         valores.append(num)
         print('Adicionado ao final da lista')
-    elif num > max(valores):
+    elif num >= max(valores):
         valores.append(num)
         print('Adicionado ao final da lista')
-    elif num < min(valores):
+    elif num <= min(valores):
         valores.insert(0, num)
         print(f'Valor adicionado na posição 0 da lista')
-    elif num > valores[0]:
+    elif num >= valores[0]:
         valores.insert(1, num)
         print(f'Valor adicionado na posição 1 da lista')
-    elif num > valores[1]:
+    elif num >= valores[1]:
         valores.insert(2, num)
         print(f'Valor adicionado na posição 3 da lista')
-    elif num > valores[2]:
+    elif num >= valores[2]:
         valores.insert(3, num)
         print(f'Valor adicionado na posição 2 da lista')
 
