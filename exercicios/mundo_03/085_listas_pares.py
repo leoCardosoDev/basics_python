13a8437114ee5b4c577ec59250bd2a3ee5809ccc
leoCardosoDev <leocardosodev@gmail.com>
2023-10-14 12:38:19 -0300

    fix: desafio 085

--- exercicios/mundo_03/085_listas_pares.py
+++ exercicios/mundo_03/085_listas_pares.py
@@ -2,20 +2,19 @@ par_e_impar = []
 pares = []
 impares = []
 
-for i in range(0,7):
-  num = int(input(f'Digite o {i+1}º valor: '))
-  if num % 2 == 0:
-    pares.append(num)
-    pares.sort()
-  else:
-    impares.append(num)
-    impares.sort()
+for i in range(0, 7):
+    num = int(input(f'Digite o {i+1}º valor: '))
+    if num % 2 == 0:
+        pares.append(num)
+        pares.sort()
+    else:
+        impares.append(num)
+        impares.sort()
 
-if pares:    
-  par_e_impar.insert(0, pares)
+if pares:
+    par_e_impar.insert(0, pares)
 if impares:
-  par_e_impar.insert(1, impares)
+    par_e_impar.insert(1, impares)
 
-par_e_impar.sort()
 print(f'Os valores pares digitados foram: {par_e_impar[0]}')
 print(f'Os valores impares digitados foram: {par_e_impar[1]}')
