4bc6475cd01af2534f7a838f9d59bd020ee5024d
leoCardosoDev <leocardosodev@gmail.com>
2023-10-14 14:42:11 -0300

    fix: desafio 088

--- exercicios/mundo_03/088_lista_mega_sena.py
+++ exercicios/mundo_03/088_lista_mega_sena.py
@@ -3,7 +3,6 @@ from time import sleep
 
 print('-'*40)
 print(f'JOGA NA MEGA SENA DA VIRADA'.rjust(45))
-jogos = []
 numeros_disponivel= list(range(1,61))
 num = int(input('Quantos jogos você quer que eu sorteie? '))
 print(f'Sorteando {num} Jogos')
@@ -11,4 +10,5 @@ print(f'Sorteando {num} Jogos')
 for i in range(0, num):
   numeros_escolhidos = sample(numeros_disponivel, 6)
   sleep(1)
+  numeros_escolhidos.sort()
   print(f'Jogo {i+1}: {numeros_escolhidos}')
