580980479487dfd50907a0a52d6fdb70a78c3fbc
leoCardosoDev <leocardosodev@gmail.com>
2023-10-14 10:34:58 -0300

    feat: desafio 087

--- exercicios/mundo_03/087_lista_matriz.py
+++ exercicios/mundo_03/087_lista_matriz.py
@@ -1,15 +1,17 @@
 matriz = []
+linha = 0
+coluna = 0
+
 matrizLinha1 = []
 matrizLinha2 = []
 matrizLinha3 = []
-linha = 0
-coluna = 0
 
 for i in range(0, 9):
     if coluna > 2:
         linha += 1
         coluna = 0
     valor = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
+
     if i <= 2:
         matrizLinha1.append(valor)
         coluna += 1
@@ -19,16 +21,27 @@ for i in range(0, 9):
     elif i >= 6:
         coluna += 1
         matrizLinha3.append(valor)
+
 matriz.insert(0, matrizLinha1)
 matriz.insert(1, matrizLinha2)
 matriz.insert(3, matrizLinha3)
+
+pares = 0
+soma3Coluna = matriz[0][2] + matriz[1][2] + matriz[2][2]
+maioValorColuna2 = max(matriz[1])
 print()
 print(f'_-'*35)
+
 for linha in matriz:
     print()
     for item in linha:
+        if item % 2 == 0:
+            pares += item
         print(f'[   {item}   ]', end='')
     print()
 print()
 print(f'_-'*35)
 print()
+print(f'A soma dos valores pares é {pares}')
+print(f'A soma dos valores da terceira coluna é {soma3Coluna}')
+print(f'O maior valor da segunda linha é {maioValorColuna2}')
