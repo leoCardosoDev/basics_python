18c6fabb181b15566b2369dac4e180aace526306
leoCardosoDev <leocardosodev@gmail.com>
2023-10-15 18:34:46 -0300

    feat: desafio 093

---
+++ exercicios/mundo_03/093_dicionarios_jogador.py
@@ -0,0 +1,23 @@
+stats = {}
+jogador = str(input('Nome do Jogador: '))
+partidas = int(input(f'Quantas partidas {jogador} jogou? '))
+gols = []
+total = 0
+
+for i in range(0, partidas):
+  gol = int(input(f'Quantos gols na partida {i}? '))
+  total += gol
+  gols.append(gol)
+
+stats['nome'] = jogador
+stats['gols'] = gols
+stats['total'] = total
+
+for key, values in stats.items():
+  print(f'O {key} tem o valor {values}')
+print(f'O jogador {jogador} jogou {partidas} partidas.')
+
+for pos, g in enumerate(stats['gols']):
+  print(f'Na partida {pos}, fez {g} gols. ')
+print(f'Fez um total de {stats["total"]} gols')
+
