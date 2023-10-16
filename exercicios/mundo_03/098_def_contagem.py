9cf91c50f4383968a641479b775fb35385f29010
leoCardosoDev <leocardosodev@gmail.com>
2023-10-16 10:27:20 -0300

    feat: desafio 098

--- exercicios/mundo_03/098_def_contagem.py
+++ exercicios/mundo_03/098_def_contagem.py
@@ -1,11 +1,20 @@
 from time import sleep
+from operator import neg, abs
 
 
 def contador(i, f, p):
     ps = str(p).replace("-", "")
     print(f'Contagem de {i} até {f} de {ps} em {ps}')
-    if f > 0:
-       f += 1
+    if f > i:
+        f += 1
+    else:
+        f -= 1
+        if p > 0:
+            p = neg(p)
+        elif f < 0:
+            p -= 1
+    if f == 0:
+        f -= f
     for index in range(i, f, p):
         print(f'{index} ', end='')
     sleep(.5)
@@ -13,7 +22,7 @@ def contador(i, f, p):
 
 
 contador(1, 10, 1)
-contador(10, 0, -2)
+contador(10, 0, 2)
 print('Agora é a sua vez de personalizar a contagem!')
 inicio = int(input('Inicio: '))
 fim = int(input('Fim: '))
