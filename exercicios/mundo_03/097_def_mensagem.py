67cd2105f42b004dc9e225d668afa4795f3bfd5a
leoCardosoDev <leocardosodev@gmail.com>
2023-10-16 08:15:49 -0300

    feat: desafio 097

---
+++ exercicios/mundo_03/097_def_mensagem.py
@@ -0,0 +1,10 @@
+def escreva(txt: str):
+    print('~'*len(txt))
+    print(f'{txt.upper():^4}')
+    print('~'*len(txt))
+
+
+escreva('Leo Silva')
+escreva('Curso de Python no Youtube')
+escreva('Aprenda a programar')
+escreva('CeV')
