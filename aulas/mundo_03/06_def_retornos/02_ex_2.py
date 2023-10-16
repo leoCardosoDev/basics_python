df39ceac6b525deeed1c62c944a63d16726d2ef1
leoCardosoDev <leocardosodev@gmail.com>
2023-10-16 17:30:41 -0300

    feat: aula def 02

--- aulas/mundo_03/06_def_retornos/02_ex_2.py
+++ aulas/mundo_03/06_def_retornos/02_ex_2.py
@@ -1,8 +1,18 @@
 def contador(inicio, fim, passo):
-  contador = inicio
-  while contador <= fim:
-    print(f'{contador} ', end='')
-    contador += passo
-  print()
+    """
+    -> Faz uma contagem e mostra na tela
+    :param inicio: inicio da contagem
+    :param fim: fim da contagem
+    :param passo: passo da contagem
+    :return: sem retorno (void)
+    """
+    contador = inicio
+    while contador <= fim:
+        print(f'{contador} ', end='')
+        contador += passo
+    print()
+
 
 contador(2, 10, 2)
+
+help(contador)
