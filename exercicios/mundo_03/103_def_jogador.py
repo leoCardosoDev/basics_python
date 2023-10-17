f8b562d2056a8f310cd6b5e918ee7dbca2960cbe
leoCardosoDev <leocardosodev@gmail.com>
2023-10-17 07:08:03 -0300

    feat: desafio 103 melhorado

--- exercicios/mundo_03/103_def_jogador.py
+++ exercicios/mundo_03/103_def_jogador.py
@@ -1,4 +1,4 @@
-def ficha(nome='', gols=0):
+def ficha(nome='<desconhecido>', gols=0):
     if nome == '':
         nome = '<desconhecido>'
     return f'O jogador {nome} fez {gols} gol(s) no campeonato'
@@ -6,7 +6,7 @@ def ficha(nome='', gols=0):
 
 jogador = str(input('Nome do jogador: '))
 gols = input('Número de gols: ')
-if gols == '':
+if gols.isnumeric() == False:
     gols = 0
 
 print(ficha(jogador, int(gols)))
