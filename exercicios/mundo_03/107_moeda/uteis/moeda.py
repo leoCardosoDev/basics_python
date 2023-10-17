7aa3b9821f62736fe853623a8cd71cc3b32881bd
leoCardosoDev <leocardosodev@gmail.com>
2023-10-17 10:10:52 -0300

    feat: desafio 107

---
+++ exercicios/mundo_03/107_moeda/uteis/moeda.py
@@ -0,0 +1,14 @@
+def aumentar(numero, porcem):
+    return numero + (porcem / 100) * numero
+
+
+def diminuir(numero, porcem):
+    return numero - (porcem / 100) * numero
+
+
+def metade(numero):
+    return numero / 2
+
+
+def dobro(numero):
+    return numero * 2
